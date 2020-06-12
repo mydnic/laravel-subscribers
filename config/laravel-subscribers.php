4fd7d9502d7366222f9f3374c8575f411aebc073
Wolfgang Hollaus <wh@rotholl.at>
2020-06-12 13:45:14 +0200

    E-mail messages in the configuration

--- config/laravel-subscribers.php
+++ config/laravel-subscribers.php
@@ -1,5 +1,24 @@
 <?php
 return [
     'verify' => env('LARAVEL_SUBSCRIBERS_VERIFY', false),
-    'redirect_url' =>'home'
+    'redirect_url' => 'home',
+    /*
+     |--------------------------------------------------------------------------
+     | Notifications Mail Messages
+     |--------------------------------------------------------------------------
+     |
+     */
+    'mail' => [
+        'verify' => [
+            'subject' => 'Verify Email Address',
+            'greeting' => 'Hello!',
+            'content' => [
+                'Please click the button below to verify your email address.'
+            ],
+            'action' => 'Verify Email Address',
+            'footer' => [
+                'If you did not sign up for our newsletter, no further action is required.'
+            ],
+        ]
+    ]
 ];
