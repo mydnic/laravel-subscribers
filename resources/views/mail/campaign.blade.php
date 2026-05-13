2cbe19fa9bdca0801d053055b72ec98738cf6a89
Clément Rigo <rigoclement@mydnic.be>
2026-05-13 10:00:06 +0200

    give more control on base template

--- resources/views/mail/campaign.blade.php
+++ resources/views/mail/campaign.blade.php
@@ -1,5 +1,26 @@
-@extends('kanpen::mail.layouts.base')
+@php
+    $isFullDocument = str_starts_with(ltrim($contentHtml), '<!DOCTYPE') || str_starts_with(ltrim($contentHtml), '<html');
+@endphp
 
-@section('content')
+@if($isFullDocument)
     {!! $contentHtml !!}
-@endsection
+@else
+    <x-mail::layout>
+        <x-slot:header>
+            <x-mail::header :url="config('app.url')">
+                {{ config('app.name') }}
+            </x-mail::header>
+        </x-slot:header>
+
+        {!! $contentHtml !!}
+
+        <x-slot:footer>
+            <x-mail::footer>
+                @isset($subscriber)
+                    You are receiving this email because you subscribed.<br>
+                    <a href="{{ $subscriber->getUnsubscribeUrl() }}">Unsubscribe</a>
+                @endisset
+            </x-mail::footer>
+        </x-slot:footer>
+    </x-mail::layout>
+@endif
